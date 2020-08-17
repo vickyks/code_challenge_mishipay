13a56a327f73351181657da9177199be49ed5507
Vicky Stephens <vicky.k.stephens@gmail.com>
2020-08-17 11:35:11 +0100

    flake8

--- shop/tests.py
+++ shop/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from shop.views import _get_resource, update_products
 
 
 class TestAPI(TestCase):
@@ -11,5 +12,16 @@ class TestAPI(TestCase):
 
     def test_api_orders(self):
         response = self.client.get('/api/load/order')
+        self.assertEqual(response.json(), {'a':'b'})
         return self.assertEqual(response.status_code, 200)
 
+    def test_get_resource__order(self):
+        response = _get_resource('orders')
+        self.assertEqual(response[0]['id'], 2300748070979)
+
+    def test_update_products(self):
+        result = update_products({})
+        return self.assertEqual(result, [])
+
+    def test_decrement_inventory(self):
+        self.assertTrue(False)
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,18 @@
+import json
+
 import requests
-from django.shortcuts import render
+from django.db.utils import IntegrityError
+from django.http import HttpResponse
+
 from mishipay_app import settings
 from shop.models import Order, Product
-from django.db.utils import IntegrityError
 
 
 def _get_resource(resource):
     username = settings.SHOPIFY_API_KEY
     password = settings.SHOPIFY_PASSWORD
-    shop_name= settings.SHOPIFY_SHOP_NAME
-    api_version=settings.SHOPIFY_API_VERSION
+    shop_name = settings.SHOPIFY_SHOP_NAME
+    api_version = settings.SHOPIFY_API_VERSION
     uri = f'https://{username}:{password}@{shop_name}.myshopify.com/admin/api/{api_version}/{resource}.json'
     response = requests.get(uri)
     return response.json()[resource]
@@ -25,43 +28,45 @@ def update_or_create(model, details):
     available_fields = set(model._meta.fields)
     aligned_fields = set(details.keys()) & available_fields
 
-    details = {k:v for k,v in details.items() if k in aligned_fields}
+    details = {k: v for k, v in details.items() if k in aligned_fields}
 
     try:
         m, created = model.objects.get_or_create(**details)
     except IntegrityError:
         # definitely exists
         m = model.objects.get(id=details['id'])
-        for k, v in items.details():
+        for k, v in details.items():
             if k != 'id':
                 setattr(m, k, v)
         m.save()
     return m
 
+
 def update_products(request):
     orders = _get_resource('orders')
-    for product in o['line_items']:
-        # XXX: should get this from InventoryLevel api
-        product.update({'inventory_level': 10})
-        p = update_or_create(Product, product)
+    for o in orders:
+        for product in o['line_items']:
+            # XXX: should get this from InventoryLevel api
+            product.update({'inventory_level': 10})
+            update_or_create(Product, product)
+
+    return HttpResponse(json.dumps(list(Product.objects.values_list('id', flat=True))))
 
 
 def load_orders(request):
     orders = _get_resource('orders')
 
     for o in orders:
-        order = update_or_create(Order, o)
+        update_or_create(Order, o)
 
         for product in o['line_items']:
             p, created = Product.objects.get_or_create(id=product['id'])
 
             if created:
-                product.inventory_level = 0
+                p.inventory_level = 0
             else:
-                product.inventory_level -=1
+                p.inventory_level -= 1
 
-            product.save()
+            p.save()
 
     return HttpResponse(json.dumps(list(Order.objects.values_list('id', flat=True))))
-
-
